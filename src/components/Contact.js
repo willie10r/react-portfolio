472c20960234cf3b1eb8b2dcdc037547e04b6df6
willie m <zx10rwillie@gmail.com>
2022-10-19 19:57:20 -0500

    upd ating the react jsx

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,17 @@ import { ReactDOM } from 'react-dom';
 
 const Contact = () => {
     return(
-        <div>whats</div>
+        <section>
+            <h2>Contact Me</h2>
+            <p>Feel Free to contact me or add me on social media</p>
+            <ul>
+                <li>email</li>
+                <li>github</li>
+                <li>instgram</li>
+                <li>frontend mentor</li>
+                <li>codewars</li>
+            </ul>
+        </section>
     )
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,16 @@ import { ReactDOM } from 'react-dom';
 
 const Projects = () => {
     return(
-        <div>whats</div>
+        <section>
+            <h2>My Personal Projects</h2>
+            <h3>Summery of skilld used and learned</h3>
+            <ul>
+                <li>This is project 1</li>
+                <li>This is project 2</li>
+                <li>This is project 3</li>
+                <li>This is project 4</li>
+            </ul>
+        </section>
     )
 };
 
