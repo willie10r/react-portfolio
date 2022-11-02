235e71deafea70f537a9b8145c0eda4b3e1e868b
willie m <zx10rwillie@gmail.com>
2022-11-01 19:41:11 -0500

    w

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,23 @@ const Contact = () => {
     return (
         <section>
             <h2 className='title'>Contact Me</h2>
+            <form>
+                <label>
+                    Name:
+                    <input type='text' value='name' />
+
+                </label>
+                <label>
+
+                    Message:
+                    <input type='text' value='message' />
+
+                </label>
+                <input type='submit' value='send message' />
+            </form>
             <p>Feel Free to contact me or add me on social media</p>
             <ul>
-                <li>email</li>
+                <li>240willie@gmail.com</li>
                 <li>github</li>
                 <li>instgram</li>
                 <li>frontend mentor</li>
